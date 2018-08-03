05c0509a0092bfb1b9318af660b1e8c40dbc4341
NHuyHoang <14110066@student.hcmute.edu.vn>
2018-08-03 13:42:16 +0700

    add screen.html button handler

--- view/script/screen.js
+++ view/script/screen.js
@@ -1,3 +1,7 @@
+var isEmergency = false;
+var isStart = false;
+var isAuto = false;
+
 function change_video(numb) {
     let source = document.getElementById("source_video");
     var video_source = "./public/videos/1.mp4"
@@ -9,13 +13,83 @@ function change_video(numb) {
     source.src = video_source;
 }
 
-function reqListener () {
+/* function reqListener() {
     console.log(this.responseText);
-  }
+} */
 
-function send_request() {
+
+function getEmergencyUrl() {
+    var emergency_btn = document.getElementById('emergency_btn');
+    isEmergency = !isEmergency;
+    if (isEmergency) {
+        emergency_btn.setAttribute('class', "btn btn-danger btn-block");
+    }
+    else {
+        emergency_btn.setAttribute('class', "btn btn-outline-danger btn-block");
+    }
+    return isEmergency ? '/screen.html?"M_E_STOP"=1' : '/screen.html?"M_E_STOP"=0';
+}
+
+function checkStart(command) {
+    var start_btn = document.getElementById('start_btn');
+    var stop_btn = document.getElementById('stop_btn');
+
+    if ((command === "start" && isStart) ||
+        (command === "stop" && !isStart)) return false;
+
+    isStart = !isStart;
+    if (isStart) {
+        start_btn.setAttribute('class', "btn btn-success btn-block");
+        stop_btn.setAttribute('class', "btn btn-outline-danger btn-block");
+    }
+    else {
+        start_btn.setAttribute('class', "btn btn-outline-success btn-block");
+        stop_btn.setAttribute('class', "btn btn-danger btn-block");
+    }
+    return true;
+}
+
+function checkAuto(command) {
+    var auto_btn = document.getElementById('auto_btn');
+    var manual_btn = document.getElementById('manual_btn');
+
+    if ((command === "auto" && isAuto) ||
+        (command === "manual" && !isAuto)) return false;
+
+    isAuto = !isAuto;
+    if (isAuto) {
+        auto_btn.setAttribute('class', "btn btn-warning btn-block");
+        manual_btn.setAttribute('class', "btn btn-outline-warning btn-block");
+    }
+    else {
+        auto_btn.setAttribute('class', "btn btn-outline-warning btn-block");
+        manual_btn.setAttribute('class', "btn btn-warning btn-block");
+    }
+    return true;
+}
+
+function send_request(command) {
     var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
-    oReq.open("GET", '/view/screen.html?"M_START"=1&"M_START"=0');
+    var url = "";
+    var prefix = "/view";
+
+    oReq.addEventListener("load", function () { });
+    switch (command) {
+        case 'start': url = '/screen.html?"M_START"=1&"M_START"=0'; break;
+        case 'stop': url = '/screen.html?"M_STOP"=1&"M_STOP"=0'; break;
+        case 'auto': url = '/screen.html?"M_MANU"=0'; break;
+        case 'manual': url = '/screen.html?"M_MANU"=1'; break;
+        case 'emergency': url = getEmergencyUrl(); break;
+    }
+
+    var makeRequest = true;
+    if (command === 'start' || command === 'stop') makeRequest = checkStart(command);
+    if (command === 'auto' || command === 'manual') makeRequest = checkAuto(command);
+
+    if (!makeRequest) return;
+
+    url = prefix + url;
+    oReq.open("GET", url);
+    console.log(url);
     oReq.send();
 }
